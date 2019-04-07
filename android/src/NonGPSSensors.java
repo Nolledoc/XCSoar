c8d9a46f3b23e5de59faea91a0ab396f0e94571b
Nolledoc <49365376+Nolledoc@users.noreply.github.com>
2019-04-07 15:10:05 +0200

    Add files via upload
    Quick and dirty hack to show vario performance of huawei mate 20 with lps22hb pressure sensor in combination with acceleration magnetometer and gyroscope sensors.
    See https://vimeo.com/328926980

--- android/src/NonGPSSensors.java
+++ android/src/NonGPSSensors.java
@@ -37,25 +37,93 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 import android.os.SystemClock;
+import android.opengl.Matrix;
 
+import java.lang.Math;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * Code to support the growing suite of non-GPS sensors on Android platforms.
  */
+
+
+
+
+
 public class NonGPSSensors implements SensorEventListener, Runnable {
   private static final String TAG = "XCSoar";
+
+/** 
+ *z2 Variables
+ */
+
+    public float[] accelerometer = new float[4];
+    public float[] a_world_frame = new float[4];
+    public float[] rotation_matrix = new float[16];
+    public float[] inverse = new float[16];
+    public float[] rotation_vector = new float[4];
+    public float pressure_z2;
+
+    public float v_ = 0.0f;
+    public float v_raw;
+    public float z;
+    public float z_raw;
+    public float z_=500f;
+    public float acc_raw;
+    public float accel = 0.0f;
+    public float dt=0.008388906f;
+    public float vario_z2;
+    public int   m =0;
+    public float acc_avg =0.0f;
+    public float acc_avg_sum =0.0f;
+
+
+    public float zVariance_ = 0.06f;
+    public float zAccelmesVariance_= 0.55f;
+    public float zAccelVariance_ = 0.00004f;
+
+    //public float zVariance_ = 0.05f;
+    //public float zAccelmesVariance_= 0.5f;
+    //public float zAccelVariance_ = 0.000025f;
+
+
+
+    public float Pzz_= 10.0f;
+    public float Pzv_= 0.0f;
+    public float Pza_= 0.0f;
+
+    public float Pvz_= 0.0f;
+    public float Pvv_= 10.0f;
+    public float Pva_= 0.0f;
+
+    public float Paz_= 0.0f;
+    public float Pav_= 0.0f;
+    public float Paa_= 10.0f;
+
+    public float innov = 0;
+    public float sInv =0;
+    public float ka=0;
+    public float kv=0;
+    public float kz=0;
+
+    public float eventval;
+    public float eventval_1;
+
+    private Sensor accel_sensor;
+    private Sensor rota_sensor;
+    private Sensor baro_sensor;
 
   // Constant array saying whether we want to support certain sensors.
   // If modifying this array, make certain that the largest ID value inside
   // is still less than SENSOR_TYPE_ID_UPPER_BOUND, and don't forget to add
   // corresponding constants to NonGPSSensors.(cpp|hpp).
   private static final int[] SUPPORTED_SENSORS = {
-    Sensor.TYPE_ACCELEROMETER,
+    Sensor.TYPE_LINEAR_ACCELERATION,
     Sensor.TYPE_GYROSCOPE,
     Sensor.TYPE_MAGNETIC_FIELD,
     Sensor.TYPE_PRESSURE,
+    Sensor.TYPE_ROTATION_VECTOR,
   // TODO: Maybe add the following sensors if we can find a use for them.
     // Sensor.TYPE_LIGHT,
     // Sensor.TYPE_PROXIMITY,
@@ -84,7 +152,7 @@ public class NonGPSSensors implements SensorEventListener, Runnable {
   // Non-inclusive upper bound on the largest sensor numerical type ID. As of
   // API 14, the largest sensor numerical type ID appears to be 13. Used only
   // for proportioning the following two arrays and for index checking.
-  private static final int SENSOR_TYPE_ID_UPPER_BOUND = 20;
+  private static final int SENSOR_TYPE_ID_UPPER_BOUND = 13;
 
   // The set of sensors in SUPPORTED_SENSORS that are present on this device,
   // i.e. that are retrieved by calling getDefaultSensor on sensor IDs present
@@ -134,9 +202,13 @@ public class NonGPSSensors implements SensorEventListener, Runnable {
 
     // Obtain the default Sensor objects for each of the desired sensors, if
     // possible. Missing sensors will yield null values.
-    for (int id : SUPPORTED_SENSORS) {
-      default_sensors_[id] = sensor_manager_.getDefaultSensor(id);
-    }
+    //for (int id : SUPPORTED_SENSORS) {
+    //  default_sensors_[id] = sensor_manager_.getDefaultSensor(id);
+    //}
+
+        accel_sensor = sensor_manager_.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        rota_sensor  = sensor_manager_.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        baro_sensor  = sensor_manager_.getDefaultSensor(Sensor.TYPE_PRESSURE);
 
     /* Attempt to identify the type of pressure sensor in this device,
        if one is present. */
@@ -242,13 +314,17 @@ public class NonGPSSensors implements SensorEventListener, Runnable {
     Log.d(TAG, "Updating non-GPS sensor subscriptions...");
     sensor_manager_.unregisterListener(this);
 
-    for (int id : SUPPORTED_SENSORS) {
-      if (enabled_sensors_[id] && default_sensors_[id] != null) {
-        Log.d(TAG, "Subscribing to sensor ID " + id + " (" + default_sensors_[id].getName() + ")");
-        sensor_manager_.registerListener(this, default_sensors_[id],
-                                         sensor_manager_.SENSOR_DELAY_NORMAL);
-      }
-    }
+    //for (int id : SUPPORTED_SENSORS) {
+    //  if (enabled_sensors_[id] && default_sensors_[id] != null) {
+    //    Log.d(TAG, "Subscribing to sensor ID " + id + " (" + default_sensors_[id].getName() + ")");
+    //    sensor_manager_.registerListener(this, default_sensors_[id],
+    //                                     sensor_manager_.SENSOR_DELAY_FASTEST);
+    //  }
+    //}
+
+        sensor_manager_.registerListener(this, accel_sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensor_manager_.registerListener(this, rota_sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        sensor_manager_.registerListener(this, baro_sensor, SensorManager.SENSOR_DELAY_FASTEST);
     Log.d(TAG, "Done updating non-GPS sensor subscriptions...");
   }
 
@@ -260,11 +336,15 @@ public class NonGPSSensors implements SensorEventListener, Runnable {
   public void onSensorChanged(SensorEvent event) {
     if (!safeDestruct.Increment())
       return;
-
     try {
       switch (event.sensor.getType()) {
-      case Sensor.TYPE_ACCELEROMETER:
+      case Sensor.TYPE_LINEAR_ACCELERATION:
         setAcceleration(event.values[0], event.values[1], event.values[2]);
+        accelerometer[0]=event.values[0];
+        accelerometer[1]=event.values[1];
+        accelerometer[2]=event.values[2];
+        accelerometer[3]=0;
+        eventval=accelerometer[0];      
         break;
       case Sensor.TYPE_GYROSCOPE:
         setRotation(event.values[0], event.values[1], event.values[2]);
@@ -273,17 +353,225 @@ public class NonGPSSensors implements SensorEventListener, Runnable {
         setMagneticField(event.values[0], event.values[1], event.values[2]);
         break;
       case Sensor.TYPE_PRESSURE:
-        setBarometricPressure(event.values[0], kf_sensor_noise_variance_);
+        //z2   0.85    0.2
+        pressure_z2 = event.values[0];
+        z_raw = sensor_manager_.getAltitude (SensorManager.PRESSURE_STANDARD_ATMOSPHERE,event.values[0]);
+	dt=0.008388906f/2.0f;
+
+        if (accel < -0.05f && v_ > 0.20)
+	{
+    		zVariance_ = 0.06f;
+    		zAccelmesVariance_= 0.30f;
+    		zAccelVariance_ = 0.000045f;
+	} else {
+		zVariance_ = 0.08f;
+    		zAccelmesVariance_= 0.35f;
+    		zAccelVariance_ = 0.000045f;
+        }
+	Predict();
+        Correct_z();
+	acc_avg_sum=acc_avg_sum+accel;
+	m=m+1;
+	if (m == 2)
+	{
+	    acc_avg=acc_avg_sum/2;
+	    acc_avg_sum=0.0f;
+	    m=0;
+	}	
+        if (acc_avg < -0.04f && v_ < 0.20f && v_ > 0.10f)
+	{
+    		vario_z2=0.04f;
+	} else {
+        vario_z2 = v_;
+	}
+        setBarometricPressure(event.values[0], kf_sensor_noise_variance_,vario_z2);
+	if (Double.isNaN(vario_z2)) {
+	    reinitialize();
+	}
+
         break;
+      case Sensor.TYPE_ROTATION_VECTOR:
+	setRotationVector(eventval_1);
+        rotation_vector[0]=event.values[0];
+        rotation_vector[1]=event.values[1];
+        rotation_vector[2]=event.values[2];
+        rotation_vector[3]=event.values[3];
+        sensor_manager_.getRotationMatrixFromVector(rotation_matrix, rotation_vector);
+        Matrix.invertM(inverse, 0, rotation_matrix, 0);
+        Matrix.multiplyMV(a_world_frame, 0, inverse, 0, accelerometer, 0);
+        acc_raw= a_world_frame[2];
+	dt=0.008388906f;
+        Predict();
+        Correct_a();
+	break;
       }
     } finally {
       safeDestruct.Decrement();
     }
   }
 
+    // reinitialize if nan
+	
+    public void reinitialize()  {
+    
+	v_ = 0.0f;
+	v_raw=0.0f;
+ 	z=0.0f;
+ 	z_raw=0.0f;
+	z_=500f;
+	acc_raw=0.0f;
+	accel = 0.0f;
+	dt=0.008388906f;
+	vario_z2=0.0f;
+
+
+	Pzz_= 10.0f;
+	Pzv_= 0.0f;
+	Pza_= 0.0f;
+
+	Pvz_= 0.0f;
+	Pvv_= 10.0f;
+	Pva_= 0.0f;
+
+	Paz_= 0.0f;
+	Pav_= 0.0f;
+	Paa_= 10.0f;
+
+	innov = 0;
+	sInv =0;
+	ka=0;
+	kv=0;
+	z=0;
+    }
+
+    // Kalman Filter z2
+
+    
+    public void Predict()   {
+
+    // Predict state
+
+    v_= v_ + accel * dt;
+    z_ = z_ + v_ * dt;
+
+
+    float dt2div2 = dt*dt/2.0f;
+    float dt3div2 = dt2div2*dt;
+    float dt4div4 = dt2div2*dt2div2;
+
+    float t11 = Pzz_ + dt*Pvz_ + dt2div2*Paz_;
+    float t12 = Pzv_ + dt*Pvv_ + dt2div2*Pav_;
+    float t13 = Pza_ + dt*Pva_ + dt2div2*Paa_;
+
+    float t21 = Pvz_ + dt*Paz_;
+    float t22 = Pvv_ + dt*Pav_;
+    float t23 = Pva_ + dt*Paa_;
+
+    float t31 = Paz_;
+    float t32 = Pav_;
+    float t33 = Paa_;
+
+    Pzz_ = t11 + dt*t12 + dt2div2*t13;
+    Pzv_ = t12 + dt*t13;
+    Pza_ = t13;
+
+    Pvz_ = t21 + dt*t22 + dt2div2*t23;
+    Pvv_ = t22 + dt*t12;
+    Pva_ = t23;
+
+    Paz_ = t31 + dt*t32 + dt2div2*t33;
+    Pav_ = t32 + dt*t33;
+    Paa_ = t33;
+
+    Pzz_ = Pzz_ + dt4div4*zAccelVariance_;
+    Pzv_ = Pzv_ + dt3div2*zAccelVariance_;
+    Pza_ = Pza_ + dt2div2*zAccelVariance_;
+
+    Pvz_ = Pvz_ + dt3div2*zAccelVariance_;
+    Pvv_ = Pvv_ + dt*dt*zAccelVariance_;
+    Pva_ = Pva_ + dt*zAccelVariance_;
+
+    Paz_ = Paz_ + dt2div2*zAccelVariance_;
+    Pav_ = Pav_ + dt*zAccelVariance_;
+    Paa_ = Paa_ + zAccelVariance_;
+
+
+    }
+
+
+
+    public void Correct_z()  {
+
+        // Error
+    innov = z_raw - z_;
+
+
+    sInv = 1.0f / (Pzz_ + zVariance_);
+
+        // Kalman gains
+    kz = Pzz_ * sInv;
+    kv = Pvz_ * sInv;
+    ka = Paz_ * sInv;
+
+
+        // Update state
+    z_ = z_ + kz * innov;
+    v_ = v_ + kv * innov;
+    accel = accel + ka * innov;
+
+        // Update state covariance matrix
+    Pzz_ = Pzz_ - kz * Pzz_;
+    Pzv_ = Pzv_ - kz * Pzv_;
+    Pza_ = Pza_ - kz * Pza_;
+
+    Pvz_ = Pvz_ - kv * Pzz_;
+    Pvv_ = Pvv_ - kv * Pzv_;
+    Pva_ = Pva_ - kv * Pza_;
+
+    Paz_ = Paz_ - ka * Pzz_;
+    Pav_ = Pav_ - ka * Pzv_;
+    Paa_ = Paa_ - ka * Pza_;
+    }
+
+    public void Correct_a()  {
+
+    // Error
+    innov = acc_raw - accel;
+
+
+    sInv = 1.0f / (Paa_ + zAccelmesVariance_);
+
+    // Kalman gains
+    kz = Pza_ * sInv;
+    kv = Pva_ * sInv;
+    ka = Paa_ * sInv;
+
+    // Update state
+    z_ = z_ + kz * innov;
+    v_ = v_ + kv * innov;
+    accel = accel + ka * innov;
+
+
+    // Update state covariance matrix
+    Pzz_ = Pzz_ - kz * Paz_;
+    Pzv_ = Pzv_ - kz * Pav_;
+    Pza_ = Pza_ - kz * Paa_;
+
+    Pvz_ = Pvz_ - kv * Paz_;
+    Pvv_ = Pvv_ - kv * Pav_;
+    Pva_ = Pva_ - kv * Paa_;
+
+    Paz_ = Paz_ - ka * Paz_;
+    Pav_ = Pav_ - ka * Pav_;
+    Paa_ = Paa_ - ka * Paa_;
+
+    }
+
   // Native methods for reporting sensor values to XCSoar's native C++ code.
   private native void setAcceleration(float ddx, float ddy, float ddz);
   private native void setRotation(float dtheta_x, float dtheta_y, float dtheta_z);
   private native void setMagneticField(float h_x, float h_y, float h_z);
-  private native void setBarometricPressure(float pressure, float sensor_noise_variance);
+  private native void setBarometricPressure(float pressure, float sensor_noise_variance, float vario_z2);
+  private native void setRotationVector(float eventval_1);
+  // z2
 }
